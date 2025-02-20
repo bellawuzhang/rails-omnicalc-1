927d7c83eef5eb8b4c7ce78b409634838f373013
bellawuzhang <bella.wu.zhang@gmail.com>
2025-02-20 16:40:47 +0000

    fixed payments

---
+++ app/controllers/math_controller.rb
@@ -0,0 +1,48 @@
+class MathController < ApplicationController
+
+  def homepage
+    render({ :template => "math_templates/homepage"})
+  end
+
+  def square_new
+    render({ :template => "math_templates/new_square_calc"})
+  end
+
+  def square_results
+    @the_num = params.fetch("users_number").to_f
+
+    @the_result = @the_num ** 2
+    render({ :template => "math_templates/square_results"})
+  end
+
+  def square_root_new
+    render({ :template => "math_templates/new_square_root_calc"})
+  end
+
+  def square_root_results
+    @the_num = params.fetch("users_number").to_f
+
+    @the_result = @the_num ** 0.5
+    render({ :template => "math_templates/square_root_results"})
+  end
+
+  def payment_new
+    render({ :template => "math_templates/new_payment_calc"})
+  end
+
+  def payment_results
+    @the_apr_input = params.fetch("apr_input").to_f
+@the_apr_input_converted = @the_apr_input.to_fs(:percentage, {:precision => 4})
+@the_years_input = params.fetch("years_input").to_f
+@the_principal_input = params.fetch("principal_input").to_f
+
+@the_apr_output = @the_apr_input / (100 * 12)
+@the_months_output = @the_years_input * 12
+@the_numerator = @the_apr_output * @the_principal_input
+@the_denominator = (1-(1 + @the_apr_output) ** -@the_months_output)
+@the_result = (@the_numerator / @the_denominator).to_fs(:currency)
+    render({ :template => "math_templates/payment_results"})
+  end
+ 
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  
+
+  get("/", { :controller => "math", :action => "homepage" })
+
+  get("/square/new", { :controller => "math", :action => "square_new" })
+
+  get("/square/results", { :controller => "math", :action => "square_results" })
+
+  get("/square_root/new", { :controller => "math", :action => "square_root_new" })
+
+  get("/square_root/results", { :controller => "math", :action => "square_root_results" })
+
+  get("/payment/new", { :controller => "math", :action => "payment_new" })
+
+  get("/payment/results", { :controller => "math", :action => "payment_results" })
+
+
 end
